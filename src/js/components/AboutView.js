02cc8696fc182c793b3b7b7fa738d4bbbdaf71fc
Rebekah <rebekah.adair@yahoo.com>
2016-03-21 09:20:51 -0400

    nested view

--- src/js/components/AboutView.js
+++ src/js/components/AboutView.js
@@ -1,14 +1,15 @@
 var AboutView = Backbone.View.extend({
 
     initialize: function () {
-        var musicView = new MusicView();
+        this.musicView = new MusicView();
     },
 
     template: _.template('<h1>About!</h1>'),
 
     render: function () {
         this.$el.html(this.template());
-        // this.musicView.setElement(this.$('.music')).render();
+        this.$el.append(this.musicView.$el);
+        this.musicView.render();
 
         return this.$el;
     }
--- src/js/components/MusicView.js
+++ src/js/components/MusicView.js
@@ -2,7 +2,7 @@ var MusicView = Backbone.View.extend({
 
     className: 'music',
 
-    template: _.template('<h1>Music!</h1>'),
+    template: _.template('<p>Music is awesome</p>'),
 
     render: function () {
         this.$el.html(this.template());
